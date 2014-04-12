5ac39a035bca69c9135714f4045cff8c0152d8c0
mexgui <mexguidev@gmail.com>
2014-04-13 01:04:17 +0300

    Now updated current work, where adding Videoplayer to the layout, long time no updates but now videplayer somehwat works and volume and seek slider.

--- MessageBoxExLib/trunk/AssemblyInfo.cpp
+++
@@ -1,6 +0,0 @@
-#include "AssemblyInfo.h"
-
-//C# TO C++ CONVERTER TODO TASK: The .NET System namespace is not available from native C++:
-
-//C# TO C++ CONVERTER TODO TASK: The .NET System namespace is not available from native C++:
-
--- mexgui/trunk/packages/tools/hdbdextractor/eac3to/VideoStreamType.cpp
+++
@@ -1,7 +0,0 @@
-#include "VideoStreamType.h"
-
-using namespace MeGUI::packages::tools::hdbdextractor;
-
-namespace eac3to
-{
-}
