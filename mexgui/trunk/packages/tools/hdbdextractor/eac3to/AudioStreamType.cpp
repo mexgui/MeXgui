cf1f1927f0b12832d78d1225d56d8001bac38185
mexgui <mexguidev@gmail.com>
2014-04-18 15:20:49 +0300

    Moved files to reference branch.

--- mexgui/trunk/packages/tools/hdbdextractor/eac3to/AudioStreamType.cpp
+++
@@ -1,9 +0,0 @@
-#include "AudioStreamType.h"
-
-//C# TO C++ CONVERTER TODO TASK: The .NET System namespace is not available from native C++:
-//using namespace System;
-using namespace MeGUI::packages::tools::hdbdextractor;
-
-namespace eac3to
-{
-}
--- mexgui/trunk/packages/tools/hdbdextractor/eac3to/StreamType.cpp
+++
@@ -1,7 +0,0 @@
-#include "StreamType.h"
-
-using namespace MeGUI::packages::tools::hdbdextractor;
-
-namespace eac3to
-{
-}
